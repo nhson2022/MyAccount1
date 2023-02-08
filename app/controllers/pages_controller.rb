6cc9d0b1bbd709f7cdfe0bcd2ecf324a96882851
nhson2022 <sonpgdlh@gmail.com>
2023-02-08 22:17:29 +0700

    add articles page for user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @articles = Article.all
   end
 
   def info
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "info" => "pages#info", as: :info
+
+  # for normal user
+  resources :articles, only: [] do
+    get "/" => "articles#index", on: :collection, as: :list
+    get "/:id" => "articles#show", on: :collection, as: :detail
+  end
+
+  # for Admin user
   devise_for :users
   namespace :admin do
     resources :articles
